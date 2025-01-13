1a0022be8a07b36090cfb0ada2fa1c63daaa7957
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-01-13 12:36:58 +0100

    feat: add a few more operator ideas without impl

---
+++ lib/src/main/kotlin/operator/basic/BashOperator.kt
@@ -0,0 +1,9 @@
+package operator.basic
+
+import operator.Operator
+
+class BashOperator(name: String) : Operator(name) {
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ lib/src/main/kotlin/operator/basic/PythonOperator.kt
@@ -0,0 +1,9 @@
+package operator.basic
+
+import operator.Operator
+
+class PythonOperator(private val name: String) : Operator(name) {
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
