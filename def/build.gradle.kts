07e72ab3402c92da75a84e223b06f8c6c482e33e
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-01-13 12:21:25 +0100

    feat: include dependencies of the library module

--- def/build.gradle.kts
+++ def/build.gradle.kts
@@ -9,6 +9,8 @@ val kotlinVersion: String by rootProject.extra
 val kotlinCoroutinesVersion: String by rootProject.extra
 
 dependencies {
+    implementation(project(":lib"))
+
     implementation("org.jetbrains.kotlin:kotlin-scripting-common:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:$kotlinVersion")
     implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:$kotlinVersion")
