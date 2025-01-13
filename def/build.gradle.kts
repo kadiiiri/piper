cc016ba5cb7bdf93078cd351c7a490513c5e671e
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-01-13 12:01:35 +0100

    feat: piper definition that can evaluate [filename].piper.kts extensions

---
+++ def/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.github"
+version = "unspecified"
+
+val kotlinVersion: String by rootProject.extra
+val kotlinCoroutinesVersion: String by rootProject.extra
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-scripting-common:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-jvm:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies-maven:$kotlinVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+    testImplementation(kotlin("test"))
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
