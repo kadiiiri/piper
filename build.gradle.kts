cfb49eddd4839d15a86ba10fbc21f0836b559099
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-01-13 12:20:41 +0100

    chore: update version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
+plugins {
+    kotlin("jvm")
+}
+
+val kubernetesVersion: String by extra("22.0.0")
+val kotlinLoggingVersion: String by extra("7.0.0")
+val slf4jSimpleVersion: String by extra("2.0.13")
 val kotlinVersion: String by extra("2.1.0")
 val kotlinCoroutinesVersion: String by extra("1.10.1")
-
 
 allprojects {
     repositories {
