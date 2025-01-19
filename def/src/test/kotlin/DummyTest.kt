15bb79b42f30acf7be302cf66fb0e7ff55d0c8ae
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-01-19 12:54:07 +0100

    test: add dummy tests to test pipeline

---
+++ def/src/test/kotlin/DummyTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class DummyTest {
+    @Test
+    fun `dummy test def works`() {
+        assertTrue { true }
+    }
+}
---
+++ lib/src/test/kotlin/pipeline/DummyTest.kt
@@ -0,0 +1,11 @@
+package pipeline
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class DummyTest {
+    @Test
+    fun `dummy test lib works`() {
+        assertTrue { true }
+    }
+}
