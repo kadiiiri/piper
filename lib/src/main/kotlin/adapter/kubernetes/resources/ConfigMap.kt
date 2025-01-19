eb7b11d20814eca9151f0dd11443200901003e83
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-01-19 13:04:53 +0100

    chore: Replace println statements with kotlin logger.

--- lib/src/main/kotlin/adapter/kubernetes/resources/ConfigMap.kt
+++ lib/src/main/kotlin/adapter/kubernetes/resources/ConfigMap.kt
@@ -1,5 +1,6 @@
 package adapter.kubernetes.resources
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.kubernetes.client.openapi.ApiException
 import io.kubernetes.client.openapi.apis.CoreV1Api
 import io.kubernetes.client.openapi.models.V1ConfigMap
@@ -7,6 +8,7 @@ import io.kubernetes.client.openapi.models.V1ObjectMeta
 import java.nio.file.Path
 
 class ConfigMap(private val coreApi: CoreV1Api, private val namespace: String) {
+    private val logger = KotlinLogging.logger {}
     fun create(configMapName: String, script: Path) {
         val configMap = V1ConfigMap()
             .metadata(V1ObjectMeta().name(configMapName))
@@ -15,7 +17,7 @@ class ConfigMap(private val coreApi: CoreV1Api, private val namespace: String) {
         try {
             coreApi.createNamespacedConfigMap(namespace, configMap).execute()
         } catch (e: ApiException) {
-            println("ConfigMap already exists: ${e.responseBody}")
+            logger.info { "ConfigMap already exists: ${e.responseBody}" }
         }
     }
 
@@ -23,7 +25,7 @@ class ConfigMap(private val coreApi: CoreV1Api, private val namespace: String) {
         try {
             coreApi.deleteNamespacedConfigMap(configMapName, namespace).execute()
         } catch (e: ApiException) {
-            println("ConfigMap does not exist: ${e.responseBody}")
+            logger.info { "ConfigMap does not exist: ${e.responseBody}" }
         }
     }
 }
