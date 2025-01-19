8ae15a1289190a4c99e9540e93c21ffc21301c87
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-01-19 12:28:37 +0100

    feat: Improve logging on the awaitCompletion of the jobmonitor

--- lib/src/main/kotlin/adapter/kubernetes/job/Monitor.kt
+++ lib/src/main/kotlin/adapter/kubernetes/job/Monitor.kt
@@ -1,32 +1,41 @@
 package adapter.kubernetes.job
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.kubernetes.client.openapi.ApiException
 import io.kubernetes.client.openapi.apis.BatchV1Api
 import kotlinx.coroutines.delay
+import kotlin.time.Duration
+import kotlin.time.DurationUnit
+import kotlin.time.toDuration
+
 
 class JobMonitor(private val batchApi: BatchV1Api, private val namespace: String) {
-    suspend fun awaitCompletion(resourceName: String) {
-        println("Waiting for completion of job with name '$resourceName'")
-        val retryInterval = 3000L // 3 seconds
+    private val logger = KotlinLogging.logger {}
+
+    suspend fun awaitCompletion(
+        resourceName: String,
+        retryInterval: Duration = 3000L.toDuration(DurationUnit.MILLISECONDS),
+    ) {
+
+        logger.info { "Waiting for completion of job with name '$resourceName'" }
 
         while (true) {
             val jobStatus = try {
                 batchApi.readNamespacedJob(resourceName, namespace).execute().status
             } catch (e: ApiException) {
-                println("Exception when reading Job '$resourceName' status: ${e.responseBody}")
-                throw e
+                logger.error {"Exception when reading Job '$resourceName' status: ${e.responseBody}"}
+                throw IllegalStateException("Job '$resourceName' failed with status: ${e.responseBody}", e)
             }
 
             when {
                 jobStatus?.succeeded?.let { it > 0 } == true -> {
-                    println("Job '$resourceName' succeeded")
+                    logger.info { "Job '$resourceName' succeeded" }
                     return
                 }
                 jobStatus?.failed?.let { it > 0 } == true -> {
-                    println("Job '$resourceName' failed")
-                    throw IllegalStateException("Job '$resourceName' failed")
+                    logger.info { "Job '$resourceName' failed" }
+                    return
                 }
-                else -> println("Job '$resourceName' is still running...")
             }
 
             delay(retryInterval)
