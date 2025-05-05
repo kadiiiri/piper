b882fe655fb55280d0bdd2cd5f94662b69d89f32
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-05-05 19:09:23 +0200

    chore: remove documentation

--- piper-web/src/main/kotlin/com/eska/piperweb/adapter/database/entities/Entities.kt
+++ piper-web/src/main/kotlin/com/eska/piperweb/adapter/database/entities/Entities.kt
@@ -6,29 +6,6 @@ import jakarta.persistence.Id
 import java.util.*
 
 
-//@Entity
-//data class Pipeline(
-//    @Id @GeneratedValue(strategy = GenerationType.UUID)
-//    val id: UUID? = null,
-//    val name: String,
-//    @OneToOne(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY)
-//    val rootOperator: Operator,
-//)
-//
-//
-//@Entity
-//data class Operator(
-//    @Id @GeneratedValue(strategy = GenerationType.UUID)
-//    val id: UUID? = null,
-//    val name: String,
-//    @OneToOne(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY)
-//    val pipeline: Pipeline,
-//    @OneToMany(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY)
-//    val children: List<Operator>,
-//    @OneToOne(cascade = [(CascadeType.ALL)], fetch = FetchType.LAZY)
-//    val parent: Operator?,
-//)
-
 @Entity
 data class Job(
     @Id
