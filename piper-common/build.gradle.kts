c5d177ea7e210b247c7e57460d3a420bb4c68cbb
Kadir Sirimsi <kadirsirimsi@gmail.com>
2025-05-18 10:24:00 +0200

    feat: unrevisioned files

---
+++ piper-common/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.github.piper"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
